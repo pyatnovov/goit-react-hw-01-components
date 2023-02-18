7da71eaed0b4d449db547b8344e83972883be70d
Vova2580 <109892815+pyatnovov@users.noreply.github.com>
2023-02-18 10:48:07 +0200

    Правки ментора

--- src/components/FriendListItem/FriendListItem.jsx
+++ src/components/FriendListItem/FriendListItem.jsx
@@ -1,14 +1,13 @@
-import PropTypes from "prop-types";
-import {Item,Status,Avatar} from "./FriendListItem.styled"
+import PropTypes from 'prop-types';
+import { Item, Status, Avatar } from './FriendListItem.styled';
 export const FriendListItem = ({ avatar, name, isOnline }) => {
-  if (isOnline === true) {
-    
-  }
-    return (<Item className="item">
-        <Status className="status" isOnline={isOnline}></Status>
-        <Avatar className="avatar" src={avatar} alt={name} width="48" />
-      <p className="name" >{name}</p>
-    </Item>);       
+  return (
+    <Item className="item">
+      <Status className="status" isOnline={isOnline}></Status>
+      <Avatar className="avatar" src={avatar} alt={name} width="48" />
+      <p className="name">{name}</p>
+    </Item>
+  );
 };
 
 FriendListItem.propTypes = {
--- src/components/Statistics/Statistics.jsx
+++ src/components/Statistics/Statistics.jsx
@@ -1,24 +1,30 @@
-import PropTypes from "prop-types";
-import { Section, List, Item, ItemInfo, Title } from "./Statistics.styled";
-import { getRandomHexColor } from "components/ProjectFunction/GetRandomColor";
+import PropTypes from 'prop-types';
+import { Section, List, Item, ItemInfo, Title } from './Statistics.styled';
+import { getRandomHexColor } from 'components/utils/GetRandomColor';
 export const Statistics = ({ title, stats }) => {
-    return <Section className="statistics">
-        <Title className="title">{title}</Title>  
-        <List className="stat-list">
-    { stats.map(stat=> (
-        <Item className="item" key={stat.id} style={{ backgroundColor:getRandomHexColor() }}>
-                <ItemInfo className="label">{stat.label}</ItemInfo>
-                <ItemInfo className="percentage">{stat.percentage}%</ItemInfo>
-            </Item>
+  return (
+    <Section className="statistics">
+      {title && <Title className="title">{title}</Title>}
+      <List className="stat-list">
+        {stats.map(stat => (
+          <Item
+            className="item"
+            key={stat.id}
+            style={{ backgroundColor: getRandomHexColor() }}
+          >
+            <ItemInfo className="label">{stat.label}</ItemInfo>
+            <ItemInfo className="percentage">{stat.percentage}%</ItemInfo>
+          </Item>
         ))}
-    </List>
+      </List>
     </Section>
+  );
 };
 
 Statistics.protoType = {
-    title: PropTypes.string.isRequired,
-    stats: PropTypes.shape({
-        label: PropTypes.string,
-        percentage:PropTypes.number,
-    }),
-}
+  title: PropTypes.string.isRequired,
+  stats: PropTypes.shape({
+    label: PropTypes.string,
+    percentage: PropTypes.number,
+  }),
+};
